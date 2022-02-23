c41d37bb16e0b311ddda474d2b56c206938842b9
Eric Muckley <ericmuckley@gmail.com>
2022-02-22 23:43:28 -0800

    Update Billboard.sol

--- contracts/Billboard.sol
+++ contracts/Billboard.sol
@@ -1,88 +1,49 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 contract Billboard {
 
-    string public message;
-    uint public nChanges;
-    uint public currentBid;
     address payable public owner;
+    mapping (address => string) public messages;
+    mapping (address => uint) public bids;
 
 
     constructor() payable {
         owner = payable(msg.sender);
-
-        nChanges = 0;
-        currentBid = 0;
-        message = 'Hello, this is the billboard.';
     }
 
-
-    // modifier for ensuring that certain contracts
-    // can only be executed by the owner address
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner may use this method");
-        _;
+    modifier onlyOwner () {
+       require(msg.sender == owner, "This function can only be called by the contract owner");
+       _;
     }
 
-    modifier validateBidAmount() {
-        require(msg.value > currentBid, "Payment must be greater than the largest previous payment");
-        _;
-    }
+    function withdraw() public payable onlyOwner {
+        owner.transfer(address(this).balance);
+    }  
 
-
-    event Payment(
-        address indexed _donor,
-        uint256 _value
+    event NewBid(
+        address indexed _bidder,
+        uint indexed _value
     );
 
+    event NewMessage(
+        address indexed _sender,
+        string indexed _message
+    );
 
-    // fallback function in case someone sends ETH
-    // directly to the contract
     fallback() external payable {
-        setMessage("message set by ", 0);
-    }
-    receive() external payable {
-        setMessage("no message sent", 0);
+        bids[msg.sender] = bids[msg.sender] + msg.value;
+        emit NewBid(msg.sender, msg.value);
     }
 
-
-    function setMessage(string memory _message, uint256 _bid) public payable {
-
-        //address(this).transfer(msg.value);
-
-        //require(msg.value > currentBid, 'Bid is not high enough');
-
-        //(bool success,) = owner.call{value: msg.value}("");
-        //require(success, "Failed to send money");
-        //require (msg.value == exactAmount);
-
-        owner.transfer(msg.value);
-        //(bool success,) = admin.call{value: msg.value, gas: _gas}("");
-
-        //require(success, "Failed to send bid with sufficient gas");
-
-        message = _message;
-        nChanges += 1;
-        currentBid = msg.value;
-
-        emit Payment(msg.sender, _bid);
-
+    receive () external payable {
+        bids[msg.sender] = bids[msg.sender] + msg.value;
+        emit NewBid(msg.sender, msg.value);
     }
 
-
-
-    function withdraw() public onlyOwner {
-        // get the amount of Ether stored in this contract
-        uint amount = address(this).balance;
-
-        (bool success, ) = owner.call{value: amount}("");
-        require(success, "Failed to send Ether");
+    function setMessage(string memory _message) public {
+        messages[msg.sender] = _message;
+        emit NewMessage(msg.sender, _message);
     }
-
 
 }
-
-
-
-
